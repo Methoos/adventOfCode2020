6397dfb692095a06d6ebb599a59eab6a22805009
Methoos <mcasdf@gmail.com>
2020-12-03 15:26:05 +0100

    day3 modified

--- src/day3/DayThree.java
+++ src/day3/DayThree.java
@@ -14,33 +14,22 @@ public class DayThree {
 		multipliedTrees *= countingTrees(7, 1, inputOfTheDay);
 		multipliedTrees *= countingTrees(1, 2, inputOfTheDay);
 		System.out.println("Result of multiplied trees: " + multipliedTrees);
-
 	}
 
 	private static int countingTrees(int moveRight, int moveDown, String[] inputOfTheDay) {
 
 		int countedTrees = 0;
 		int positionOfTheTraveler = 0;
-		boolean success = false;
 		int row = 0;
-		while (!success) {
-			if (row < inputOfTheDay.length - moveDown) {
-				if (positionOfTheTraveler + moveRight < inputOfTheDay[row].length()) {
-					positionOfTheTraveler += moveRight;
-				} else {
-					positionOfTheTraveler = (moveRight - 1) - (inputOfTheDay[row].length() - 1 - positionOfTheTraveler);
-				}
-				row+=moveDown;
-				if ("#".equals(inputOfTheDay[row].substring(positionOfTheTraveler, positionOfTheTraveler + 1))) {
-					countedTrees++;
-				}
-			} else {
-				success = true;
+		while (row < inputOfTheDay.length - moveDown) {
+			if ('#' == inputOfTheDay[row].charAt(positionOfTheTraveler)) {
+				countedTrees++;
 			}
+			positionOfTheTraveler = (positionOfTheTraveler + moveRight) % inputOfTheDay[row].length();
+			row += moveDown;
 		}
 		System.out.println("The result is: " + countedTrees + " trees for right=" + moveRight + " and down=" + moveDown
 				+ " excercise.");
-
 		return countedTrees;
 	}
 
