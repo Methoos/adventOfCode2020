4d3e9141f926072a9b14c8312f353e504439c093
Methoos <mcasdf@gmail.com>
2020-12-03 13:22:02 +0100

    day3firstexcercise committed

--- src/day3/DayThree.java
+++ src/day3/DayThree.java
@@ -6,10 +6,25 @@ public class DayThree {
 
 	public static void main(String[] args) {
 		String[] inputOfTheDay = InputReader.read("src/day3/day3.txt", "");
+		int countedTrees = 0;
+		int positionOfTheTraveler = 0;
+		boolean success = false;
+		int row = 0;
 
-		for (int i = 0; i < inputOfTheDay.length; i++) {
-			System.out.println(inputOfTheDay[i]);	
+		while (!success) {
+			if (row < inputOfTheDay.length - 1) {
+				if (positionOfTheTraveler + 3 < inputOfTheDay[row].length()) {
+					positionOfTheTraveler += 3;
+				} else {
+					positionOfTheTraveler = 2 - (inputOfTheDay[row].length() - 1 - positionOfTheTraveler);
+				}
+				if ("#".equals(inputOfTheDay[++row].substring(positionOfTheTraveler, positionOfTheTraveler + 1))) {
+					countedTrees++;
+				}
+			} else {
+				success = true;
+			}
 		}
-		System.out.println(inputOfTheDay.length);
+		System.out.println("The result is: "+ countedTrees+" trees.");
 	}
 }
