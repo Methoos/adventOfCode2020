e6c097b4a77bf120bd28040e0e7df03ecda26297
Methoos <mcasdf@gmail.com>
2020-12-03 13:40:50 +0100

    day3modified, excercise 2 done

--- src/day3/DayThree.java
+++ src/day3/DayThree.java
@@ -6,25 +6,42 @@ public class DayThree {
 
 	public static void main(String[] args) {
 		String[] inputOfTheDay = InputReader.read("src/day3/day3.txt", "");
+		long multipliedTrees = 1L;
+
+		multipliedTrees *= countingTrees(1, 1, inputOfTheDay);
+		multipliedTrees *= countingTrees(3, 1, inputOfTheDay);
+		multipliedTrees *= countingTrees(5, 1, inputOfTheDay);
+		multipliedTrees *= countingTrees(7, 1, inputOfTheDay);
+		multipliedTrees *= countingTrees(1, 2, inputOfTheDay);
+		System.out.println("Result of multiplied trees: " + multipliedTrees);
+
+	}
+
+	private static int countingTrees(int moveRight, int moveDown, String[] inputOfTheDay) {
+
 		int countedTrees = 0;
 		int positionOfTheTraveler = 0;
 		boolean success = false;
 		int row = 0;
-
 		while (!success) {
-			if (row < inputOfTheDay.length - 1) {
-				if (positionOfTheTraveler + 3 < inputOfTheDay[row].length()) {
-					positionOfTheTraveler += 3;
+			if (row < inputOfTheDay.length - moveDown) {
+				if (positionOfTheTraveler + moveRight < inputOfTheDay[row].length()) {
+					positionOfTheTraveler += moveRight;
 				} else {
-					positionOfTheTraveler = 2 - (inputOfTheDay[row].length() - 1 - positionOfTheTraveler);
+					positionOfTheTraveler = (moveRight - 1) - (inputOfTheDay[row].length() - 1 - positionOfTheTraveler);
 				}
-				if ("#".equals(inputOfTheDay[++row].substring(positionOfTheTraveler, positionOfTheTraveler + 1))) {
+				row+=moveDown;
+				if ("#".equals(inputOfTheDay[row].substring(positionOfTheTraveler, positionOfTheTraveler + 1))) {
 					countedTrees++;
 				}
 			} else {
 				success = true;
 			}
 		}
-		System.out.println("The result is: "+ countedTrees+" trees.");
+		System.out.println("The result is: " + countedTrees + " trees for right=" + moveRight + " and down=" + moveDown
+				+ " excercise.");
+
+		return countedTrees;
 	}
+
 }
