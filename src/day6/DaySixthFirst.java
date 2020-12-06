6dabaa58b22ee38dba85a3f7ce41646dedc01277
Methoos <mcasdf@gmail.com>
2020-12-06 11:41:04 +0100

    day6 uploaded

--- src/day6/DaySixthFirst.java
+++ src/day6/DaySixthFirst.java
@@ -1,13 +1,29 @@
 package day6;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import inputReader.InputReader;
 
 public class DaySixthFirst {
 	public static void main(String[] args) {
-		String[] inputOfTheDay = InputReader.read("src/day6/day6.txt", "");	
+		String[] inputOfTheDay = InputReader.read("src/day6/day6.txt", "");
+		List<String> list = new ArrayList<String>();
+		int counter = 0;
 		for (int i = 0; i < inputOfTheDay.length; i++) {
-			System.out.println(inputOfTheDay[i]);
+			if ((!"".equals(inputOfTheDay[i]))) {
+				String memberOfGroup = inputOfTheDay[i];
+				for (int j = 0; j < memberOfGroup.length(); j++) {
+					if (!list.contains(memberOfGroup.substring(j, j + 1))) {
+						list.add(memberOfGroup.substring(j, j + 1));
+					}
+				}
+			} else {
+				counter += list.size();
+				list.clear();
+			}
 		}
-		System.out.println(inputOfTheDay.length);
+		counter += list.size();
+		System.out.println("There were " + counter + " \"yes\" answers.");
 	}
 }
--- src/day6/DaySixthSecond.java
+++ src/day6/DaySixthSecond.java
@@ -1,13 +1,41 @@
 package day6;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
 import inputReader.InputReader;
 
 public class DaySixthSecond {
 	public static void main(String[] args) {
-		String[] inputOfTheDay = InputReader.read("src/day6/day6.txt", "");	
+		String[] inputOfTheDay = InputReader.read("src/day6/day6.txt", "");
+		List<String> list = new ArrayList<String>();
+		int counter = 0;
+		boolean isFirstMemberInGroup = true;
 		for (int i = 0; i < inputOfTheDay.length; i++) {
-			System.out.println(inputOfTheDay[i]);
+			if ((!"".equals(inputOfTheDay[i]))) {
+				String memberOfGroup = inputOfTheDay[i];
+				ListIterator<String> it = list.listIterator();
+				if (!isFirstMemberInGroup) {
+					while (it.hasNext()) {
+						String target = it.next();
+						if (!memberOfGroup.contains(target)) {
+							it.remove();
+						}
+					}
+				} else {
+					for (int j = 0; j < memberOfGroup.length(); j++) {
+						list.add(memberOfGroup.substring(j, j + 1));
+					}
+					isFirstMemberInGroup = false;
+				}
+			} else {
+				counter += list.size();
+				list.clear();
+				isFirstMemberInGroup = true;
+			}
 		}
-		System.out.println(inputOfTheDay.length);
+		counter += list.size();
+		System.out.println("The result is: " + counter);
 	}
 }
