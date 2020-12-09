b690955498de8d077c0fe3640261f6d004049a3e
Methoos <mcasdf@gmail.com>
2020-12-09 10:52:22 +0100

    day9 first solution added

--- src/day9/DayNinthFirst.java
+++ src/day9/DayNinthFirst.java
@@ -1,13 +1,58 @@
 package day9;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import inputReader.InputReader;
 
 public class DayNinthFirst {
 	public static void main(String[] args) {
 		String[] inputOfTheDay = InputReader.read("src/day9/day9.txt", "");
+		final int preamble = 25;
+		List<Long> numbers = createLongFromLines(inputOfTheDay);
+
+		System.out.println(searchForTheNumber(numbers, preamble) + " is the searched number.");
+	}
+
+	private static List<Long> createLongFromLines(String[] inputOfTheDay) {
+		List<Long> numbers = new ArrayList<Long>();
 		for (int i = 0; i < inputOfTheDay.length; i++) {
-			System.out.println(inputOfTheDay[i]);
+			numbers.add(Long.parseLong(inputOfTheDay[i]));
 		}
-		System.out.println(inputOfTheDay.length);
+		return numbers;
+	}
+
+	private static long searchForTheNumber(List<Long> Numbers, int preamble) {
+		for (int numb = preamble; numb < Numbers.size(); numb++) {
+			if (!searchInSubList(Numbers, Numbers.get(numb), numb, preamble)) {
+				return Numbers.get(numb);
+			}
+		}
+		return -1;
+	}
+
+	private static boolean searchInSubList(List<Long> numbers, long searchedNumber, int numb, int preamble) {
+		for (int i = numb - preamble; i < numb - 1; i++) {
+			if (searchInSubSubList(numbers.get(i), numbers, i, numb, searchedNumber)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private static boolean searchInSubSubList(long firstNumb, List<Long> numbers, int i, int numb,
+			long searchedNumber) {
+		int j = i + 1;
+		while (j < numb) {
+			if (checkSum(firstNumb, numbers.get(j), searchedNumber, numbers)) {
+				return true;
+			}
+			j++;
+		}
+		return false;
+	}
+
+	private static boolean checkSum(long firstNumb, long secondNumber, long searchedNumber, List<Long> numbers) {
+		return searchedNumber == firstNumb + secondNumber;
 	}
 }
