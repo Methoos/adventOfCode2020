24e519749c31772367999179ecab291dd190a3a7
Methoos <mcasdf@gmail.com>
2020-12-10 09:49:28 +0100

    day10 first solved

--- src/day10/DayTenthFirst.java
+++ src/day10/DayTenthFirst.java
@@ -1,13 +1,39 @@
 package day10;
 
+import java.util.Set;
+import java.util.TreeSet;
+
 import inputReader.InputReader;
 
 public class DayTenthFirst {
 	public static void main(String[] args) {
 		String[] inputOfTheDay = InputReader.read("src/day10/day10.txt", "");
-		for (int i = 0; i < inputOfTheDay.length; i++) {
-			System.out.println(inputOfTheDay[i]);
+		Integer[] adapters = makeSortedIntegerArrayFromLines(inputOfTheDay);
+		System.out.println("Multiplied 1 and 3 differences is: "+multipleDifferences(adapters));
+	}
+
+	private static int multipleDifferences(Integer[] adapters) {
+		int rating = 0;
+		int diffOne = 0;
+		int diffThree = 0;
+		for (int i = 0; i < adapters.length; i++) {
+			if ((adapters[i] - rating) == 1) {
+				diffOne++;
+			}
+			if ((adapters[i] - rating) == 3) {
+				diffThree++;
+			}
+			rating = adapters[i];
 		}
-		System.out.println(inputOfTheDay.length);
+		diffThree++;
+		return diffOne * diffThree;
+	}
+
+	private static Integer[] makeSortedIntegerArrayFromLines(String[] inputOfTheDay) {
+		Set<Integer> adapters = new TreeSet<>();
+		for (int line = 0; line < inputOfTheDay.length; line++) {
+			adapters.add(Integer.parseInt(inputOfTheDay[line]));
+		}
+		return adapters.toArray(new Integer[adapters.size()]);
 	}
 }
