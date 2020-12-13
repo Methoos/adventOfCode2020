0e366981b723859f275752edd310cac4e1410722
Methoos <mcasdf@gmail.com>
2020-12-13 20:09:55 +0100

    day14, day15 skeleton added

--- src/day12/DayTwelveSecond.java
+++ src/day12/DayTwelveSecond.java
@@ -4,7 +4,7 @@ import inputReader.InputReader;
 
 public class DayTwelveSecond {
 	public static void main(String[] args) {
-		String[] inputOfTheDay = InputReader.read("src/day12/day12.txt", "");
+		String[] inputOfTheDay = InputReader.read("src/day12/example.txt", "");
 		for (int i = 0; i < inputOfTheDay.length; i++) {
 			System.out.println(inputOfTheDay[i]);
 		}
