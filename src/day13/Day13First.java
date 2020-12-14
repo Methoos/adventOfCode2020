cfda9a1e4d3bb24c8beb9637875494ce14dd4725
Methoos <mcasdf@gmail.com>
2020-12-14 09:53:37 +0100

    day13 first solution added

--- src/day13/Day13First.java
+++ src/day13/Day13First.java
@@ -5,9 +5,25 @@ import inputReader.InputReader;
 public class Day13First {
 	public static void main(String[] args) {
 		String[] inputOfTheDay = InputReader.read("src/day13/day13.txt", "");
-		for (int i = 0; i < inputOfTheDay.length; i++) {
-			System.out.println(inputOfTheDay[i]);
+		System.out
+				.println("Earliest bus ID multiplied waiting minutes: " + findEarliestBus(inputOfTheDay));
+	}
+
+	private static int findEarliestBus(String[] inputOfTheDay) {
+		final int startTimeStamp = Integer.parseInt(inputOfTheDay[0]);
+		int solution = 0;
+		int min = Integer.MAX_VALUE;
+		for (int busID = 1; busID < inputOfTheDay.length; busID++) {
+			int ID = Integer.parseInt(inputOfTheDay[busID]);
+			int diff = calcTimestampDiff(ID, startTimeStamp);
+			if (diff < min) {
+				solution = diff * ID; min = diff;
+			}
 		}
-		System.out.println(inputOfTheDay.length);
+		return solution;
+	}
+
+	private static int calcTimestampDiff(int busID, int startTimeStamp) {
+		return ((startTimeStamp / busID) + 1) * busID - startTimeStamp;
 	}
 }
--- src/day13/Day13Second.java
+++ src/day13/Day13Second.java
@@ -4,7 +4,7 @@ import inputReader.InputReader;
 
 public class Day13Second {
 	public static void main(String[] args) {
-		String[] inputOfTheDay = InputReader.read("src/day13/day13.txt", "");
+		String[] inputOfTheDay = InputReader.read("src/day13/examples.txt", "");
 		for (int i = 0; i < inputOfTheDay.length; i++) {
 			System.out.println(inputOfTheDay[i]);
 		}
